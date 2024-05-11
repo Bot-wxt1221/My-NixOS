8310346276c905b3e5961404484c9cb219dd64b2
wxt <3264117476@qq.com>
2024-05-11 11:43:42 +0800

    Update

---
+++ nixos/configuration.nix
@@ -0,0 +1,163 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.device = "nodev";
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_rt_6_8;
+  services.flatpak.enable = true;
+  boot.loader.grub.efiSupport = true;
+  networking.hostName = "wxt"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  virtualisation.waydroid.enable = true;
+  qt.enable = true;
+  qt.platformTheme = "gtk2";
+  qt.style = "gtk2";
+  nix.settings.experimental-features = [ "nix-command" ];
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://127.0.0.1:7897/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "zh_CN.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     useXkbConfig = true; # use xkb.options in tty.
+   };
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+        libsForQt5.fcitx5-qt
+        fcitx5-gtk
+	fcitx5-chinese-addons
+    ];
+};
+fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-cjk
+  noto-fonts-emoji
+  liberation_ttf
+  fira-code
+  fira-code-symbols
+  mplus-outline-fonts.githubRelease
+  dina-font
+  proggyfonts
+  jetbrains-mono
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+];
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.wxt = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "adbusers"]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+    ];
+    
+  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  services.todesk.enable = true;
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    nano
+    thefuck
+    waydroid
+    xclip todesk
+    gcc (lowPrio clang) (lowPrio clang-tools) gdb gnumake lld
+    wget
+    cmake
+    home-manager alsa-firmware
+    gnome.gnome-terminal
+    linuxKernel.kernels.linux_rt_6_8
+    gnome.gnome-tweaks
+    clash-verge-rev
+  ];
+  systemd.services.clash-verge-rev = {
+    enable = true;
+    description = "clash verge rev";
+    serviceConfig = {
+      ExecStart = "${pkgs.clash-verge-rev}/lib/clash-verge/resources/clash-verge-service";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+  nixpkgs.config.allowUnfree = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "unstable"; # Did you read the comment?
+  nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store"];
+}
