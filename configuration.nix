5bd48589de4150efa836cf079954c2b6dcdffa50
wxt <3264117476@qq.com>
2024-05-19 14:24:46 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -63,15 +63,14 @@
         # 所以直接使用它很可能会报错
         #
         # pkgs.appimageTools 提供了大多数程序常用的基础包，所以我们可以直接用它来补充
-        (base.targetPkgs pkgs) ++ with pkgs; [
-          pkg-config
-	  appimageTools
+        (base.targetPkgs pkgs) ++ with pkgs; [ 
+	  pkg-config
           ncurses
           # 如果你的 FHS 程序还有其他依赖，把它们添加在这里
         ]
       );
       profile = "export FHS=1";
-      runScript = "bash";
+      runScript = "zsh";
       extraOutputsToInstall = ["dev"];
     }))
     gcc (lowPrio clang) (lowPrio clang-tools) gdb gnumake lld vscode-extensions.ms-vscode.cpptools
