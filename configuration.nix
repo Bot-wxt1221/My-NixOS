a7ebfeb286e3d3e8497fc8aa73e0c5dd151454a6
wxt <3264117476@qq.com>
2024-05-19 14:25:40 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -63,11 +63,9 @@
         # 所以直接使用它很可能会报错
         #
         # pkgs.appimageTools 提供了大多数程序常用的基础包，所以我们可以直接用它来补充
-        (base.targetPkgs pkgs) ++ with pkgs; [ 
 	  pkg-config
           ncurses
           # 如果你的 FHS 程序还有其他依赖，把它们添加在这里
-        ]
       );
       profile = "export FHS=1";
       runScript = "zsh";
