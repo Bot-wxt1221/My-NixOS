db1f8232bcb3e5ac33424aaa1afdb4ec74394700
wxt <3264117476@qq.com>
2024-05-20 21:54:04 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -117,5 +117,7 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "unstable"; # Did you read the comment?
-  nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store"];
+  nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store" "https://nix-community.cachix.org"];
+  nix.settings.trusted-users = ["root" "wxt"];
+  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="]
 }
