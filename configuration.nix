259035db9bd5d4c4444fdc928b6fa3e37d146b9a
wxt <3264117476@qq.com>
2024-05-20 21:56:32 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -119,5 +119,5 @@
   system.stateVersion = "unstable"; # Did you read the comment?
   nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store" "https://nix-community.cachix.org"];
   nix.settings.trusted-users = ["root" "wxt"];
-  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="]
+  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
 }
