768580d25c183f413d7dbad8c83d219f8c6732ef
wxt <3264117476@qq.com>
2024-05-23 18:05:07 +0800

    Update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -49,6 +49,7 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
+       offload = {
        	enable = true;
 			enableOffloadCmd = true;
 		};
