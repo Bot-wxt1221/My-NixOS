4ab196efe6c3c639a62d03f4b003739fa476ed06
wxt <3264117476@qq.com>
2024-05-23 19:01:04 +0800

    Update

--- home-wxt.nix
+++ home-wxt.nix
@@ -125,7 +125,7 @@
     userEmail = "3264117476@qq.com";
   };
 
-  home.stateVersion = "24.05";
+  home.stateVersion = "24.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
