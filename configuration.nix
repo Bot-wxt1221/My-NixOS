b57c9f64676412bd67fa65aa9b923422f4c5fbed
wxt <3264117476@qq.com>
2024-05-28 22:57:28 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,13 @@
     enableGC = false;
     autoMount = true;
   };
+  virtualisation.kvmgt.enable = true;
+  virtualisation.kvmgt.vgpus = {
+    "i915-GVTg_V5_8" = {
+      uuid = [ "53227488-1d02-11ef-be24-d3abbba451ea" ];
+    };
+  };
+
   virtualisation.docker.enable = true;
   boot.loader.grub.device = "nodev";
   boot.kernelPackages = pkgs.linuxKernel.packages.linux_rt_6_9;
