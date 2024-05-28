8af136b83573d0d96f0872e2ae0809a55b3d68cc
wxt <3264117476@qq.com>
2024-05-28 21:38:49 +0800

    Update

--- home-wxt.nix
+++ home-wxt.nix
@@ -64,7 +64,7 @@
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc  # it is a calculator for the IPv4/v6 addresses
-    alsa-firmware
+    alsa-firmware libreoffice-fresh 
     # misc
     cowsay
     file
