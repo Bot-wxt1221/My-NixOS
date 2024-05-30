08fb65db20e54a77b64f8213c82921a8a69cdf5c
wxt <3264117476@qq.com>
2024-05-30 23:44:38 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,9 @@
 
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
   boot.loader.efi.canTouchEfiVariables = true;
   security.sudo.wheelNeedsPassword = false;
   services.printing.drivers = [ pkgs.hplipWithPlugin ];
