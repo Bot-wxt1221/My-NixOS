ce76b8bdc2edc99c21a1125e42df57afe1cf9766
wxt <3264117476@qq.com>
2024-06-14 09:16:04 +0800

    Update

--- system/laptop.nix
+++ system/laptop.nix
@@ -23,17 +23,6 @@
     };
   };
   services.thermald.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-     governor = "powersave";
-     turbo = "never";
-    };
-    charger = {
-     governor = "performance";
-     turbo = "auto";
-    };
-  };
   powerManagement.powertop.enable = true;
 }
 
