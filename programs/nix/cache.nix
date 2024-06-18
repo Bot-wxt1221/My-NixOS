f2406f48454ca7408842b18d4224f5c0a25ead7c
wxt <3264117476@qq.com>
2024-06-18 21:41:26 +0800

    Update

--- programs/nix/cache.nix
+++ programs/nix/cache.nix
@@ -6,5 +6,5 @@
   system.stateVersion = "unstable"; # Did you read the comment?
   nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store" "https://nix-community.cachix.org" "https://wxt1221.cachix.org"];
   nix.settings.trusted-users = ["root" "wxt"];
-  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "wxt1221.cachix.org-1:KR3i2n9P8aIYE5a7+nUOHLhxz4jQ2IvdVUncYmvHXqM="];
+  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "wxt1221.cachix.org-1:JmeZ+5reEo1i5qBc6X+2hEadoN5GFqTOQcwaGD5PEwY="];
 }
