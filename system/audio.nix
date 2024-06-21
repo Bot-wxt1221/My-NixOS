1cf0e1c42c9652378dfe833dcf154d17a6cba091
wxt <3264117476@qq.com>
2024-06-21 21:15:15 +0800

    Update

--- system/audio.nix
+++ system/audio.nix
@@ -22,22 +22,22 @@
       {
         name = "libpipewire-module-protocol-pulse";
         args = {
-          pulse.min.req = "256/48000";
-          pulse.default.req = "256/48000";
-          pulse.max.req = "256/48000";
-          pulse.min.quantum = "256/48000";
-          pulse.max.quantum = "256/48000";
+          pulse.min.req = "2560/48000";
+          pulse.default.req = "2560/48000";
+          pulse.max.req = "2560/48000";
+          pulse.min.quantum = "2560/48000";
+          pulse.max.quantum = "2560/48000";
         };
       }
     ];
     context.properties = {
       default.clock.rate = 48000;
-      default.clock.quantum = 256;
-      default.clock.min-quantum = 256;
-      default.clock.max-quantum = 256;
+      default.clock.quantum = 2560;
+      default.clock.min-quantum = 2560;
+      default.clock.max-quantum = 2560;
     };
     stream.properties = {
-      node.latency = "256/48000";
+      node.latency = "2560/48000";
       resample.quality = 1;
     };
   };
