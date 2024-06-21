4ae78514660c59826bf2c38c757c9714eca26cc8
wxt <3264117476@qq.com>
2024-06-21 20:41:37 +0800

    Update

--- system/audio.nix
+++ system/audio.nix
@@ -22,22 +22,22 @@
       {
         name = "libpipewire-module-protocol-pulse";
         args = {
-          pulse.min.req = "32/48000";
-          pulse.default.req = "32/48000";
-          pulse.max.req = "32/48000";
-          pulse.min.quantum = "32/48000";
-          pulse.max.quantum = "32/48000";
+          pulse.min.req = "128/48000";
+          pulse.default.req = "128/48000";
+          pulse.max.req = "128/48000";
+          pulse.min.quantum = "128/48000";
+          pulse.max.quantum = "128/48000";
         };
       }
     ];
     context.properties = {
       default.clock.rate = 48000;
-      default.clock.quantum = 32;
-      default.clock.min-quantum = 32;
-      default.clock.max-quantum = 32;
+      default.clock.quantum = 128;
+      default.clock.min-quantum = 128;
+      default.clock.max-quantum = 128;
     };
     stream.properties = {
-      node.latency = "32/48000";
+      node.latency = "128/48000";
       resample.quality = 1;
     };
   };
