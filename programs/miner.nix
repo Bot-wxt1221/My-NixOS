37fb1e4003124380292641e45467867dff6a8b2d
wxt <3264117476@qq.com>
2024-06-21 22:21:59 +0800

    Update

--- programs/miner.nix
+++ programs/miner.nix
@@ -7,5 +7,5 @@
     pkgs.xmrig
   ];
   services.xmrig.enable = true;
-  services.xmrig.settings = libs.fromJSON (libs.readFile "miner.json");
+  services.xmrig.settings = lib.fromJSON (lib.readFile "miner.json");
 }
