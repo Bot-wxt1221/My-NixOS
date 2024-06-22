ef3353d5826a1c133a3cd262cba72134036cd1af
wxt <3264117476@qq.com>
2024-06-22 17:52:42 +0800

    Update

--- system/kernel.nix
+++ system/kernel.nix
@@ -3,9 +3,9 @@
   imports = [
 
   ];
-  boot.kernelPackages = pkgs.linuxKernel.kernels.linux_zen;
+  boot.kernelPackages = pkgs.linuxKernel.kernels.linux_zen_latest;
   environment.systemPackages = with pkgs; [
-    pkgs.linuxKernel.kernels.linux_zen
+    pkgs.linuxKernel.kernels.linux_zen_latest
   ];
   boot.kernelParams = [
     "acpi_osi=Linux-Dell-Video"
