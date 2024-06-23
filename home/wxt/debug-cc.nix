a5905d5e84fc94dae24fc555a9ea5e3fe348bdf4
wxt <3264117476@qq.com>
2024-06-24 07:44:51 +0800

    Update

--- home/wxt/debug-cc.nix
+++ home/wxt/debug-cc.nix
@@ -3,40 +3,9 @@
   imports = [
 
   ];
-  home.file.".local/share/nvim/lazy/NvChad/lua/nvchad/configs/dap/init.lua".text = ''
-    local M = {}
-
-local function configure()
-end
-
-local function configure_exts()
-end
-
-local function configure_debuggers()
-end
-
-function M.setup()
-	configure() -- Configuration
-	configure_exts() -- Extensions
-	configure_debuggers() -- Debugger
-  require("nvchad.configs.dap.keymap").setup()
-  require("nvchad.configs.dap.ui").setup()
-  require("nvchad.configs.dap.icon").setup()
---  require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp", "rust" } })
-  local dap = require('dap')
-  dap.adapters.cppdbg = {
-    id = 'cppdbg',
-    type = 'executable',
-    command = '${pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7',
-  }
-end
-
-configure_debuggers()
-
-return M
-
-  '';
-  home.file."OI/.vscode/launch.json".text = ''
+  home.file."".source = ;
+  home.file.".local/share/OpenDebugAD7/OpenDebugAD7".text = '${pkgs.vscode-extensions.ms-vscode.cpptools}/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7';
+  home.file.".local/share/OpenDebugAD7/OpenDebugAD7".executable = true;
 {
     "version": "0.2.0",
     "configurations": [
