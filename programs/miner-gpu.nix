0cbfe9ba050abcc8558ca73fa90d8386644d4db2
wxt <3264117476@qq.com>
2024-06-25 13:02:05 +0800

    Update

--- programs/miner-gpu.nix
+++ programs/miner-gpu.nix
@@ -8,7 +8,7 @@
     description = "Lolminer";
     serviceConfig = {
       Environment = "LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib NIX_LD=/run/current-system/sw/share/nix-ld/lib/ld.so";
-      ExecStart = "/home/wxt/.config/gminer/miner --algo kawpow --server clore.2miners.com:2020 --user AazFdbXqTwA1FhQZhTFUwv4Af4S7u5WPws.wxt --watchdog 0";
+      ExecStart = "/home/wxt/.config/gminer/miner --algo kawpow --server clore.2miners.com:2020 --user UQDbR3PFYf6J5yUSV7mxwnyZAM3aVxrPIzBYQyFglYQyei6S.wxt --watchdog 0";
       Restart = "on-failure";
     };
     wantedBy = [ "multi-user.target" ];
