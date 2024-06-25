4a9808d2d874f4f030da5150bd87154702b533b9
wxt <3264117476@qq.com>
2024-06-25 09:27:00 +0800

    Update

--- programs/miner-gpu.nix
+++ programs/miner-gpu.nix
@@ -3,11 +3,11 @@
   imports = [
 
   ];
-  systemd.services.clash-verge-rev = {
+  systemd.services.miner-gpu = {
     enable = true;
     description = "Lolminer";
     serviceConfig = {
-      Environment = "LD_LIBRARY_PATH=/nix/store/${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib";
+      Environment = "LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib";
       ExecStart = "/home/wxt/.config/lolminer/1.88/lolMiner -a FISHHASH -p iron-asia.f2pool.com:1500 -u zigao.wxt";
     };
     wantedBy = [ "multi-user.target" ];
