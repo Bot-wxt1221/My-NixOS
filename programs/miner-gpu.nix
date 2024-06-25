d2e49c7a9c797de919d4379e3a4592606e0f79cc
wxt <3264117476@qq.com>
2024-06-25 10:06:22 +0800

    Update

--- programs/miner-gpu.nix
+++ programs/miner-gpu.nix
@@ -8,7 +8,8 @@
     description = "Lolminer";
     serviceConfig = {
       Environment = "LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.cudaPackages.cudatoolkit}/lib NIX_LD=/run/current-system/sw/share/nix-ld/lib/ld.so";
-      ExecStart = "/home/wxt/.config/lolminer/1.88/lolMiner -a FISHHASH -p iron-asia.f2pool.com:1500 -u zigao.wxt";
+      ExecStart = "/home/wxt/.config/gminer/miner --algo etchash --server stratum-etc.antpool.com:8008 --user 0x4218E7fF553587087556fe8d1DeC3FCd71d0fb3c.wxt  --watchdog 0";
+      Restart = "on-failure";
     };
     wantedBy = [ "multi-user.target" ];
   };
