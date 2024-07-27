1b17a690fb73182acde2a7d5e6ca92fcfce65655
wxt <3264117476@qq.com>
2024-07-27 23:44:12 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Main config";
 
   inputs = {
-    nixpkgs.url = "github:Bot-wxt1221/nixpkgs/main";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     neovim-src.url = "github:neovim/neovim";
