bf8133552cb99c2fca51c23ca36438e83da97c96
wxt <3264117476@qq.com>
2024-07-29 21:37:18 +0800

    Update

--- configuration.nix
+++ configuration.nix
@@ -15,32 +15,6 @@
       ./programs/all.nix
       ./system/all.nix
     ];
-systemd.services.todeskd = {
-      description = "ToDesk Daemon Service";
-
-      wantedBy = [ "multi-user.target" ];
-      wants = [
-        "network-online.target"
-	"display-manager.service"
-        "nss-lookup.target"
-      ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.todesk}/bin/todesk service";
-        ExecReload = "${pkgs.coreutils}/bin/kill -SIGINT $MAINPID";
-        Restart = "on-failure";
-        User = "root";
-        Group = "root";
-#        WorkingDirectory = "/var/lib/todesk";
-#        StateDirectory = "todesk";
-#        StateDirectoryMode = "0777"; # Desktop application read /opt/todesk/config/config.ini. Such a pain!
-#        ProtectSystem = "strict";
-#        ProtectHome = "read-only";
-#        PrivateTmp = "yes";
-#        RemoveIPC = "yes";
-      };
-    };
-  environment.systemPackages = [pkgs.todesk];
   time.timeZone = "Asia/Shanghai";
   users.users.wxt = {
     isNormalUser = true;
