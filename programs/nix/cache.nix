d1f23f988c464eff871f5e7531f6b20247511ee0
wxt <3264117476@qq.com>
2024-08-05 15:21:52 +0800

    Update

--- programs/nix/cache.nix
+++ programs/nix/cache.nix
@@ -1,10 +1,27 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
 
   ];
   system.stateVersion = "unstable"; # Did you read the comment?
-  nix.settings.substituters = ["https://mirrors.ustc.edu.cn/nix-channels/store" "https://nix-community.cachix.org" "https://wxt1221.cachix.org" "https://cuda-maintainers.cachix.org"];
-  nix.settings.trusted-users = ["root" "wxt"];
-  nix.settings.trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "wxt1221.cachix.org-1:JmeZ+5reEo1i5qBc6X+2hEadoN5GFqTOQcwaGD5PEwY=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
+  nix.settings.substituters = [
+    "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://nix-community.cachix.org"
+    "https://wxt1221.cachix.org"
+    "https://cuda-maintainers.cachix.org"
+  ];
+  nix.settings.trusted-users = [
+    "root"
+    "wxt"
+  ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "wxt1221.cachix.org-1:JmeZ+5reEo1i5qBc6X+2hEadoN5GFqTOQcwaGD5PEwY="
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+  ];
 }
--- programs/nix/nix.nix
+++ programs/nix/nix.nix
@@ -1,8 +1,12 @@
-{ config, lib, pkgs, nixpkgs, ... }:
 {
-  imports = [
-    ./cache.nix
-  ];
+  config,
+  lib,
+  pkgs,
+  nixpkgs,
+  ...
+}:
+{
+  imports = [ ./cache.nix ];
   nix.optimise.automatic = true;
   nix.optimise.dates = [ "12:00" ]; # Optional; allows customizing optimisation schedule
   nix.gc = {
@@ -10,7 +14,10 @@
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   nixpkgs.config.allowUnfree = true;
   nix.registry.nixpkgs.flake = nixpkgs;
   environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
