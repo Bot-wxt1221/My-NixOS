de0f332ef099a3364766fea3622b463f6f4ee525
wxt <3264117476@qq.com>
2024-08-06 14:20:51 +0800

    Update

--- home/wxt/hyprland/swaync.nix
+++ home/wxt/hyprland/swaync.nix
@@ -6,6 +6,6 @@
   home.packages = with pkgs;[
     swaynotificationcenter
   ];
-  xdg.configFile."swaync/style.css".source = ./style.css;
-  xdg.configFile."swaync/config.json".source = ./config.json;
+  xdg.configFile."swaync/style.css".source = ./swaync.css;
+  xdg.configFile."swaync/config.json".source = ./swaync.json;
 }
