411a2090c74f4225d1a95c66451a2f75ff8ebe4e
wxt <3264117476@qq.com>
2024-08-06 13:26:52 +0800

    Update

--- home/wxt/hyprland.nix
+++ home/wxt/hyprland.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
-
+    ./hyprpaper.nix
   ];
-  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-  home.file.".config/hypr/wallpaper.png".source = ./nixos-wallpaper-catppuccin-latte.png;
-  home.file.".config/hypr/hyprpaper.conf".text = builtins.readFile ./hyprpaper.conf;
+  home.file.".config/hypr/hyprland.conf".text = ''
+    
+  '';
 }
