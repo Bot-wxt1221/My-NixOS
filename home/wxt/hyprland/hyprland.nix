a2ee6b27be627e58bc6dec062cdb1c52dabb312e
wxt <3264117476@qq.com>
2024-08-06 23:21:03 +0800

    Update

--- home/wxt/hyprland/hyprland.nix
+++ home/wxt/hyprland/hyprland.nix
@@ -236,7 +236,8 @@
 
       bind=,escape,submap,reset
     submap=reset
-
+    bind=$mainMod,L,switch:off:Lid Switch,exec,hyprctl keyword monitor "eDP-1, preferred, auto, auto"
+    bind=$mainMod,l,switch:on:Lid Switch,exec,hyprctl keyword monitor "eDP-1, disable"
     bind=SUPERSHIFT,h,movewindow,l
     bind=SUPERSHIFT,l,movewindow,r
     bind=SUPERSHIFT,j,movewindow,u
