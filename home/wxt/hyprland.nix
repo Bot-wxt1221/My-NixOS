128a81917b38d04d4a7396065e131abfe4e481d6
wxt <3264117476@qq.com>
2024-08-06 13:24:59 +0800

    Update

--- home/wxt/hyprland.nix
+++ home/wxt/hyprland.nix
@@ -5,5 +5,5 @@
   ];
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config/hypr/wallpaper.png".source = ./nixos-wallpaper-catppuccin-latte.png;
-  home.file.".config/hypr/hyprpaper.conf".text = builtins.readFile "./hyprpaper.conf";
+  home.file.".config/hypr/hyprpaper.conf".text = builtins.readFile ./hyprpaper.conf;
 }
