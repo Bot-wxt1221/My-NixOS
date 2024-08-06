df5ac405ecf102cd7d6ebcc3f5cb98b860e1cb50
wxt <3264117476@qq.com>
2024-08-06 13:29:41 +0800

    Update

---
+++ home/wxt/hyprpaper.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+
+  ];
+  home.file.".config/hypr/wallpaper.png".source = ./nixos-wallpaper-catppuccin-latte.png;
+  home.file.".config/hypr/hyprpaper.conf".text = ''
+    perload = ${config.home.homeDirectory}/.config/hypr/wallpaper.png
+    wallpaper = ,${config.home.homeDirectory}/.config/hypr/wallpaper.png
+  '';
+}
