f003f1578726277ca04348307825fd709b670d00
wxt <3264117476@qq.com>
2024-08-09 08:41:25 +0800

    Update

--- home/wxt/hyprland/hyprland.nix
+++ home/wxt/hyprland/hyprland.nix
@@ -88,6 +88,8 @@
     env = XDG_SESSION_TYPE,wayland
     env = GBM_BACKEND,nvidia-drm
     env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+    env = GTK_IM_MODULE, fcitx5
+    env = QT_IM_MODULE, fcitx5
 
     cursor {
       no_hardware_cursors = true
