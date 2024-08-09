0f76aae39e59b46b71a51ccdcf3fbaf16816d676
wxt <3264117476@qq.com>
2024-08-09 08:50:09 +0800

    Update

--- home/wxt/hyprland/hyprland.nix
+++ home/wxt/hyprland/hyprland.nix
@@ -183,7 +183,6 @@
       workspace_swipe = false
     }
 
-    exec-once=fcitx5 --replace -d
     exec-once=waydroid session start
     exec-once=sleep 5 && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" set-default && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" mute && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" unmute
     exec-once=sleep 5 && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" set-default && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" mute && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" unmute
--- programs/fcitx5.nix
+++ programs/fcitx5.nix
@@ -8,12 +8,14 @@
   imports = [
 
   ];
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       libsForQt5.fcitx5-qt
+      fcitx5-rime
       fcitx5-gtk
       fcitx5-chinese-addons
     ];
