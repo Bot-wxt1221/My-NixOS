7755cf43c78efe3a74c43abb918efb6d9d310f20
wxt <3264117476@qq.com>
2024-08-11 16:11:06 +0800

    ypd

--- build-school-vmware.sh
+++ build-school-vmware.sh
@@ -1,3 +1,3 @@
 sudo nixos-rebuild build --flake .#wxt-school-vmware --log-format internal-json -v |& nom --json
 sudo nixos-rebuild build --flake .#wxt-school-vmware
-nix-store --export $(nix-store --query --requisites ./result) | pigz -c > /run/media/wxt/526E-46BF/nix-closure-export.storeobject.gz
+nix-store --export $(nix-store --query --requisites ./result) | pigz -c > /run/media/wxt/Ventoy/nix-closure-export.storeobject.gz
