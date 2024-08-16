a758707a1e18bbd27bfeeb8bfe06eece55fb4703
wxt <3264117476@qq.com>
2024-08-16 22:29:28 +0800

    Update

--- programs/nix-ld.nix
+++ programs/nix-ld.nix
@@ -10,17 +10,23 @@
   ];
   programs.nix-ld.enable = true;
   programs.nix-ld.package = pkgs.nix-ld-rs;
-  programs.nix-ld.libraries = with pkgs; [
-    fuse3
-    curl
-    gtk3
-    openssl
-    fuse2
-    ocl-icd
-    glib
-    nss
-    lib.mkIf
-    config.hardware.nvidia.modesetting.enable
-    (cudaPackages.cudatoolkit linuxPackages.nvidia_x11)
-  ];
+  programs.nix-ld.libraries = with pkgs;
+    lib.mkMerge [
+      [
+        fuse3
+        curl
+        gtk3
+        openssl
+        fuse2
+        ocl-icd
+        glib
+        nss
+      ]
+      (lib.mkIf
+      config.hardware.nvidia.modesetting.enable
+      [
+        cudaPackages.cudatoolkit
+        linuxPackages.nvidia_x11
+      ])
+    ];
 }
