2ae94eedb9c8002142331775218e049ac00ce86c
wxt <3264117476@qq.com>
2024-08-20 22:02:25 +0800

    Update

--- programs/nix-ld.nix
+++ programs/nix-ld.nix
@@ -10,18 +10,14 @@
   ];
   programs.nix-ld.enable = true;
   programs.nix-ld.package = pkgs.nix-ld-rs;
-  programs.nix-ld.libraries =
-    with pkgs;
-    lib.mkMerge [
-      [
-        fuse3
-        curl
-        gtk3
-        openssl
-        fuse2
-        ocl-icd
-        glib
-        nss
-      ]
-    ];
+  programs.nix-ld.libraries = with pkgs; [
+    fuse3
+    curl
+    gtk3
+    openssl
+    fuse2
+    ocl-icd
+    glib
+    nss
+  ];
 }
