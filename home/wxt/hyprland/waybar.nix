4a628e766f2c24bda481debaabfff517209d8c9a
wxt <3264117476@qq.com>
2024-08-29 21:38:58 +0800

    Update

--- home/wxt/hyprland/waybar.nix
+++ home/wxt/hyprland/waybar.nix
@@ -64,7 +64,7 @@
           months = "<span color='#ffead3'><b>{}</b></span>";
           days = "<span color='#ecc6d9'><b>{}</b></span>";
           weeks = "<span color='#99ffdd'><b>W{}</b></span>";
-          weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+          weekdays = "<span color='#ffcc66'><b>{}&nbsp;</b></span>";
           today = "<span color='#ff6699'><b><u>{}</u></b></span>";
         };
       };
