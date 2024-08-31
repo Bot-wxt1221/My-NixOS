844b891cf31437f87569dcded41b53a10b0cce1a
wxt <3264117476@qq.com>
2024-08-31 16:52:45 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Main config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     neovim-src.url = "github:neovim/neovim";
