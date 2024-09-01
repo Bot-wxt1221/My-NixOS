7e2cffa268dc9b50afb9cd70c50fa3a7ace8ff34
wxt <3264117476@qq.com>
2024-09-01 10:20:16 +0800

    Update

--- programs/fcitx5.nix
+++ programs/fcitx5.nix
@@ -14,7 +14,9 @@
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       libsForQt5.fcitx5-qt
+      fcitx5-with-addons
       fcitx5-gtk
+      fcitx5-rime
       fcitx5-chinese-addons
     ];
     fcitx5.waylandFrontend = true;
