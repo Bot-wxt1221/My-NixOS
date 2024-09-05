c9db86f2683c05a3643b054f602f1a29d2508a3f
wxt <3264117476@qq.com>
2024-09-05 11:47:59 +0800

    Update

--- home/wxt/hyprland/hyprland.nix
+++ home/wxt/hyprland/hyprland.nix
@@ -204,6 +204,7 @@
       exec-once=sleep 5 && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" set-default && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" mute && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" unmute
       exec-once=sleep 5 && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" set-default && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" mute && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" unmute
       exec-once=clash-verge
+      exec-once=vmhgfs-fuse .host:/ /mnt/ -o allow_other 
       exec-once=hyprpaper
       exec-once=waybar
       exec-once=wl-clip-persist --clipboard regular
