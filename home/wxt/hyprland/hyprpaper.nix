e5c2b4a5dda974d705bfbd3f7ec7b4fd3c5b7864
wxt <3264117476@qq.com>
2024-09-22 16:38:10 +0800

    Update

--- home/wxt/hyprland/hyprpaper.nix
+++ home/wxt/hyprland/hyprpaper.nix
@@ -9,11 +9,14 @@
   imports = [
 
   ];
-  home.file.".config/hypr/wallpaper.jpeg".source = ./wallpaper.jpeg;
+  home.file.".config/hypr/wallpaper.svg".source = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/refs/heads/master/wallpapers/nixos-wallpaper-catppuccin-latte.svg";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
   services.hyprpaper.enable = true;
   services.hyprpaper.settings = {
-    preload = [ "${config.home.homeDirectory}/.config/hypr/wallpaper.jpeg" ];
-    wallpaper = [ ",${config.home.homeDirectory}/.config/hypr/wallpaper.jpeg" ];
+    preload = [ "${config.home.homeDirectory}/.config/hypr/wallpaper.svg" ];
+    wallpaper = [ ",${config.home.homeDirectory}/.config/hypr/wallpaper.svg" ];
   };
   services.hyprpaper.package = pkgs.hyprpaper;
   home.packages = with pkgs; [ hyprpaper ];
