4fb37fac362e708adce6eb09e6967ca6911e63da
wxt <3264117476@qq.com>
2024-10-01 09:16:00 +0800

    Update

--- g3-configuration.nix
+++ g3-configuration.nix
@@ -47,32 +47,32 @@
     packages = with pkgs; [ ];
   };
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  # specialisation = {
-  #   test-kernel = {
-  #     configuration = {
-  #       system.nixos.tags = [ "test-kernel" ];
-  #       boot.kernelPackages = pkgs.linuxPackagesFor (
-  #         pkgs.buildLinux {
-  #           version = "6.11-rc2";
-  #           extraMeta.branch = "6.11";
-  #           kernelPatches = [
-  #             {
-  #               name = "bridge-stp-helper";
-  #               patch = "${nixpkgs}/pkgs/os-specific/linux/kernel/bridge-stp-helper.patch";
-  #             }
-  #             {
-  #               name = "request-key-helper";
-  #               patch = "${nixpkgs}/pkgs/os-specific/linux/kernel/request-key-helper.patch";
-  #             }
-  #           ];
-  #           src = pkgs.fetchzip {
-  #             url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-6.11-rc2.tar.gz";
-  #             hash = "sha256-+nfCerBqzOtf3xYVQfXA24r557V7v0Pe9J9p/R0DXOs=";
-  #           };
-  #         }
-  #       );
-  #     };
-  #     inheritParentConfig = true;
-  #   };
-  # };
+  specialisation = {
+    test-kernel = {
+      configuration = {
+        system.nixos.tags = [ "test-kernel" ];
+        boot.kernelPackages = pkgs.linuxPackagesFor (
+          pkgs.buildLinux {
+            version = "6.12-rc1";
+            extraMeta.branch = "6.12";
+            kernelPatches = [
+              {
+                name = "bridge-stp-helper";
+                patch = "${nixpkgs}/pkgs/os-specific/linux/kernel/bridge-stp-helper.patch";
+              }
+              {
+                name = "request-key-helper";
+                patch = "${nixpkgs}/pkgs/os-specific/linux/kernel/request-key-helper.patch";
+              }
+            ];
+            src = pkgs.fetchzip {
+              url = "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-6.12-rc1.tar.gz";
+              hash = "sha256-+nfCerBqzOtf3xYVQfXA24r557V7v0Pe9J9p/R8DXOs=";
+            };
+          }
+        );
+      };
+      inheritParentConfig = true;
+    };
+  };
 }
