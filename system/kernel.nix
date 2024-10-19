fecd4f8782e52af3c8ca784d162af6c1256d880c
wxt <3264117476@qq.com>
2024-10-19 19:39:56 +0800

    Update

--- system/kernel.nix
+++ system/kernel.nix
@@ -8,6 +8,6 @@
   imports = [
 
   ];
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_stable;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
   hardware.firmware = [ pkgs.linux-firmware ];
 }
