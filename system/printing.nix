67cdc2c33cfc16235b00164c5df3f4cbcab4c977
wxt <3264117476@qq.com>
2024-12-27 17:39:43 +0800

    tt

--- system/printing.nix
+++ system/printing.nix
@@ -8,6 +8,18 @@
   imports = [
 
   ];
-  services.printing.drivers = [ pkgs.hplipWithPlugin ];
+  services.printing.drivers = [
+    (pkgs.hplipWithPlugin.overrideAttrs (old: {
+      env = {
+        NIX_CFLAGS_COMPILE = toString [
+          "-Wno-error=implicit-int"
+          "-Wno-error=implicit-function-declaration"
+          "-Wno-error=return-mismatch"
+          "-Wno-error=int-conversion"
+          "-Wno-error=incompatible-pointer-types"
+        ];
+      };
+    }))
+  ];
   services.printing.enable = true;
 }
