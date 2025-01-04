b84e6c8d688ff1801136dfde79e5db5bd0645799
wxt <3264117476@qq.com>
2025-01-04 20:58:21 +0800

    Update

--- programs/nix-ld.nix
+++ programs/nix-ld.nix
@@ -9,14 +9,64 @@
 
   ];
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    fuse3
-    curl
-    gtk3
-    openssl
-    fuse2
-    ocl-icd
-    glib
-    nss
-  ];
+  programs.nix-ld.libraries =
+    with pkgs;
+    [
+      acl
+      attr
+      bzip2
+      dbus
+      expat
+      fontconfig
+      freetype
+      fuse3
+      icu
+      libnotify
+      libsodium
+      libssh
+      libunwind
+      libusb1
+      libuuid
+      nspr
+      nss
+      stdenv.cc.cc
+      util-linux
+      zlib
+      zstd
+    ]
+    ++ lib.optionals (config.hardware.graphics.enable) [
+      pipewire
+      cups
+      libxkbcommon
+      pango
+      mesa
+      libdrm
+      libglvnd
+      libpulseaudio
+      atk
+      cairo
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      gdk-pixbuf
+      glib
+      gtk3
+      libGL
+      libappindicator-gtk3
+      vulkan-loader
+      xorg.libX11
+      xorg.libXScrnSaver
+      xorg.libXcomposite
+      xorg.libXcursor
+      xorg.libXdamage
+      xorg.libXext
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXtst
+      xorg.libxcb
+      xorg.libxkbfile
+      xorg.libxshmfence
+    ];
 }
