5e045bbf5cfa2f1e6ac3cd725d6bb02c3fcb5f8e
wxt <3264117476@qq.com>
2025-01-26 20:54:37 +0800

    Update

--- hardware/g3.nix
+++ hardware/g3.nix
@@ -55,7 +55,7 @@
     powerManagement.enable = true;
     powerManagement.finegrained = true;
     open = true;
-    nvidiaSettings = false;
+    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
     prime = {
       offload = {
