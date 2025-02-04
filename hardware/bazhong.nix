ebf1fe47c84bca62116ffc7ae43e96b8d2c66991
wxt <3264117476@qq.com>
2025-02-04 17:52:23 +0800

    Update

--- hardware/bazhong.nix
+++ hardware/bazhong.nix
@@ -47,9 +47,11 @@
   ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [
-    (rtl8821ce.override {
-      stdenv = pkgs.gcc13Stdenv;
-    })
+    (rtl8821ce.overrideAttrs (old: {
+      env.NIX_CFLAGS_COMPILE = toString [
+        "-Wno-error=incompatible-pointer-types"
+      ];
+    }))
     (acpi_call.overrideAttrs (old: {
       preBuild = (old.preBuild or "") + "export buildRoot=.";
     }))
