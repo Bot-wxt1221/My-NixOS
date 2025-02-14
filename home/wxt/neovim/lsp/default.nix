9b925f222d7a9a4679b6a01a4f2b498900b06186
wxt <3264117476@qq.com>
2025-02-15 07:44:18 +0800

    Update

--- home/wxt/neovim/lsp/default.nix
+++ home/wxt/neovim/lsp/default.nix
@@ -35,6 +35,29 @@
 
   programs.nixvim.extraConfigLua = # lua
     ''
+      -- shamelessly copied from: https://github.com/NvChad/ui/blob/v2.5/lua/nvchad/lsp/init.lua
+      local function lspSymbol(name, icon)
+        local hl = "DiagnosticSign" .. name
+        vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+      end
+
+      lspSymbol("Error", "󰅙")
+      lspSymbol("Info", "󰋼")
+      lspSymbol("Hint", "󰌵")
+      lspSymbol("Warn", "")
+
+      vim.diagnostic.config {
+        virtual_text = {
+          prefix = "",
+        },
+        signs = true,
+        underline = true,
+
+        float = {
+          border = "single",
+        },
+      }
+
       --  LspInfo window borders
       local win = require "lspconfig.ui.windows"
       local _default_opts = win.default_opts
