f81e2bb7e7514f89c10e2ad0dc1a8c5901eeaae8
wxt <3264117476@qq.com>
2025-02-19 11:44:15 +0800

    Update

--- home/wxt/secret/default.nix
+++ home/wxt/secret/default.nix
@@ -12,5 +12,9 @@
     ./2FA
   ];
   sops.age.keyFile = osConfig.sops.age.keyFile;
-
+  # Only for trusted user!
+  home.activation.copy-age-key = ''
+    mkdir -p ${config.home.homeDirectory}/.config/sops/age
+    cp -rf ${config.sops.age.keyFile} ${config.home.homeDirectory}/.config/sops/age/keys.txt
+  '';
 }
