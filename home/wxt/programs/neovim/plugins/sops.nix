832c607f218fa8e75eeb12d463800d2dd2d3c299
wxt <3264117476@qq.com>
2025-02-19 11:51:53 +0800

    Update

--- home/wxt/programs/neovim/plugins/sops.nix
+++ home/wxt/programs/neovim/plugins/sops.nix
@@ -9,5 +9,12 @@
   programs.nixvim.extraPlugins = with pkgs.vimPlugins; [
     nvim-sops
   ];
-  programs.nixvim.extraConfigLuaPost = "require('nvim_sops').setup()";
+  programs.nixvim.extraConfigLuaPost = ''
+    require("nvim_sops").setup({
+      enabled = true,
+      debug = true,
+    })
+    vim.keymap.set("n", "<leader>ef", vim.cmd.SopsEncrypt, { desc = "[E]ncrypt [F]ile" })
+    vim.keymap.set("n", "<leader>df", vim.cmd.SopsDecrypt, { desc = "[D]ecrypt [F]ile" })
+  '';
 }
