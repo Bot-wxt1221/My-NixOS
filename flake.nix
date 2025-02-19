a6f6c3583fbadc317fa5d5523af9691cc642af3f
wxt <3264117476@qq.com>
2025-02-19 21:21:34 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -4,37 +4,38 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-colors.url = "github:misterio77/nix-colors";
+    luogu-gcc.url = "github:luogu-dev/judge-env";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     impermanence = {
       url = "github:nix-community/impermanence";
     };
-
-    neovim.url = "github:nix-community/neovim-nightly-overlay/master";
-    neovim.inputs.nixpkgs.follows = "nixpkgs";
-
+    neovim = {
+      url = "github:nix-community/neovim-nightly-overlay/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixVim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-    nix-colors.url = "github:misterio77/nix-colors";
-
-    luogu-gcc.url = "github:luogu-dev/judge-env";
     clipboard = {
       url = "github:Bot-wxt1221/clipboard-sync";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     niri = {
       url = "github:sodiboo/niri-flake/8bfb763f07fd9106e032590d071c30450e3b80f6";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-stable.follows = "nixpkgs";
     };
-
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sosp-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
