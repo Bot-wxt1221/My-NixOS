18f15f4b3734ebf38b4f6fd973248f3fd5539b32
wxt <3264117476@qq.com>
2025-02-21 12:44:37 +0800

    Update

--- programs/flatpak.nix
+++ programs/flatpak.nix
@@ -12,9 +12,11 @@
   environment.persistence."/persist".directories = [ "/var/lib/flatpak" ];
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
+    after = [ "display-manager.service" ];
     path = [ pkgs.flatpak ];
     serviceConfig.Type = "oneshot";
     script = ''
+      sleep 10
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
