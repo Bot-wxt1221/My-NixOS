953a1b0fb90cd8798856543685584fa29fdf93d7
wxt <3264117476@qq.com>
2025-02-22 15:33:53 +0800

    Update

---
+++ programs/chromium/extensions.nix
@@ -0,0 +1,73 @@
+{ pkgs, lib, ... }:
+[
+  {
+    id = "kpbnombpnpcffllnianjibmpadjolanh";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5oiT4dhXdTA7WDRw0Nud9W9Wi928GYoz4phma1YQHik957A4602tFMysQI25u7SixzvTGvpd5162Yu7DAWA_Fw7ewVMFsYTZXm1PFk-A9z1XLUbD2jq-aM80XgxMqyOeAMZSmuWnLeX0cnAwvDZpFTy3-gQj8RBXmw/KPBNOMBPNPCFFLLNIANJIBMPADJOLANH_2_8_19_0.crx";
+      hash = "sha256-r1Gw3PngCjVscR1cdV/Cp5cqxZU/XnQ/3hQlpXOxiiY=";
+    };
+  }
+  {
+    id = "fpnmgdkabkmnadcjpehmlllkndpkmiak";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5oiiV3rxkK_L76y4QiayRkx6ZCh2eR5oM_8VNQDWL__V1HvfUPnQFE5jO3QcPn629NF2ntMTjVhZgy1Zzg4O8_I_Fg39mHs_gQrXCTTGICsJdrDK-FSeMEMB2eLLBpsAxlKa5WWsoYoeRaX5kItTRAQtd3B1boJc/FPNMGDKABKMNADCJPEHMLLLKNDPKMIAK_3_4_2_0.crx";
+      hash = "sha256-PSeBNFEN2dsC/FX2IigksnghOLLtXVZJI/nDYVZfz34=";
+    };
+  }
+  {
+    id = "bfbmjmiodbnnpllbbbfblcplfjjepjdn";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5oiDu-rAYk2gl1qSPXAIpp_CY3Lxh96vUD8iR7nWPhYq4uoZyjaZAPaUfBd8gC9lWnfZ_XZnpdCZ59RmxmkgOlHZwmyZ0yYQSQ_T9cIlGz4-LV-ga3lOor5-W0D6DXfzAMZSmuWpVeYxi3p7l0tbWABHR7L9kPVN8g/BFBMJMIODBNNPLLBBBFBLCPLFJJEPJDN_4_5_7_0.crx";
+      hash = "sha256-1L3D7lpMqlwlAkFH/qhkSgSQOp+aCBA/uzXrkFF6mOQ=";
+    };
+  }
+  {
+    id = "dhdgffkkebhmkfjojejmpbldmpobfkfo";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ohCXiirA90u1VZjf225-dNm_gBouCaCe_kQDrCEUzVg8zXYmSAMHH276W02qCoiXcrBjUr65z2GV3nJi9cIT2rM5kA2Au_WPA1FTHD61KMRdJm6tKlbyFIemy-Anf8AxlKa5X09FijTg_4HFAx1PGQIkZXPh_iS/DHDGFFKKEBHMKFJOJEJMPBLDMPOBFKFO_5_3_3_0.crx";
+      hash = "sha256-ovAHUGgcJBX4tELTvSi7LS3zDSIK/v/2Wyz7vONsrs4=";
+    };
+  }
+  {
+    id = "ajkhmmldknmfjnmeedkbkkojgobmljda";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ojRcBzVgjX_V0SONpyPFflpSbv_NJjzXJ0HuWwE7Yjg5Zyy-ZcrqXOTwZB4bTfM91Sq9WkeKeKoK9TYuiVH3h3bPxUs2cfVawVA_RwzEsyiUyJmFMTBQuFthbJWWXIAxlKa5XzGinf7d_OyNqpcZwV8iQAHNK57/AJKHMMLDKNMFJNMEEDKBKKOJGOBMLJDA_1_5_8_0.crx";
+      hash = "sha256-SCv8JoXeA9Qzv5mHjlO9vNWaMDFVdOFCsXalvX7XxTM=";
+    };
+  }
+  {
+    id = "gakohpplicjdhhfllilcjpfildodfnnn";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ogYGgXJT3JCrFXLr_0nsvdt_BKGtdhory0KvbqCENjrp-rcfN-t6yUCTpr_yFkWZuz_c8amLBYkT-wfn83CIYonlNkipkEfMvrS6hEU7x4N-jgQQKCafzxt8iAnFPwAxlKa5Zhoo6I-6mpm17epx63GT315sPpS/GAKOHPPLICJDHHFLLILCJPFILDODFNNN_0_6_7_0.crx";
+      hash = "sha256-CLNPlcvIW3kHDUWKoNTZh3m7nzgd6bdc+5MlfW9jtDc=";
+    };
+  }
+  {
+    id = "cjnmckjndlpiamhfimnnjmnckgghkjbl";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ohKsBGRWEClnkeq83ZVHKYfEh8lPbyOFHY7RuSzmuPU_JpW5nJeJ-UHt0aNoh0JpQuwMmhfGT7ui_rutlaV4UBxrheJFvdvkmUlaYxV92-ujtpROlGqM83DPm93vtMAxlKa5XnbsS0exhOXXenMjZc5tFpPhRZr/CJNMCKJNDLPIAMHFIMNNJMNCKGGHKJBL_2_60_0_0.crx";
+      hash = "sha256-pN/5ci/hF4UQHjHJzHrGdXiiuupwKPXYvuLPoZv7KPM=";
+    };
+  }
+  {
+    id = "oboonakemofpalcgghocfoadofidjkkk";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ogG7icbEuMbY4H3Q7EUFzUOw098MQYcD3Yjg01T4ThUHo3eebdEcg3q_JRHoKwmichptevluDxBQ9XAq7NJDS5oEzBy4fvkhhnGtkrZWnbVben5MVz5TGdTPQUXLDAAxlKa5RoruKfe5urGaYib7xYOQki4dCd_/OBOONAKEMOFPALCGGHOCFOADOFIDJKKK_1_9_6_0.crx";
+      hash = "sha256-raxuvQ0g/IqJWtU5bepqoGKMN+/UaYR5r4VIUU5pSus=";
+    };
+  }
+  {
+    id = "jnbbnacmeggbgdjgaoojpmhdlkkpblgi";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ojkseLmCtDekRtWvCcF5Z0uNcJzyz9zNmcRZt0G95ViXGpBLGSyTXKKF-johkMbMi2kDZCWbfnK54d67mUGMFbAhTednNUX-n192djH-i_6yAHNOsQigdhog66znaVKAMZSmuV-HHZtjwYXFwf8hNNcsOS0j6yq3Q/JNBBNACMEGGBGDJGAOOJPMHDLKKPBLGI_4_0_12_0.crx";
+      hash = "sha256-oWLxI8y8XQhTii/ZVNEOwXBpj6QnMiX3UTwlgX/Up+E=";
+    };
+  }
+  {
+    id = "aapbdbdomjkkjkaonfhkkikfgjllcleb";
+    crxPath = pkgs.fetchurl {
+      url = "https://clients2.googleusercontent.com/crx/blobs/ASuc5ohgUWm2z_9wWaSgkesUK4VDWA3_V-pbKkkv-ZN-zhJlPcjrpZieXUJ7NmxghdZ74RvJR5EUSRunCROmDmIHhoKdJIxW2FkraD1v3DaoqgQ-XtYKxqbKex3qNCoHUk0AxlKa5UQpvCDITd3016ZVXpttbeRC6Tln/AAPBDBDOMJKKJKAONFHKKIKFGJLLCLEB_2_0_16_0.crx";
+      hash = "sha256-Z05TixCLsTuLjrm+e3T39tPMIu4buFw93Nm/lxpF3AI=";
+    };
+  }
+]
--- programs/chromium/update.sh
+++ programs/chromium/update.sh
@@ -6,7 +6,7 @@ do
   line=$(echo $line2 | awk -F " " '{print $1}')
   finalurl=$(curl -w "%{url_effective}\n" -I -L -s -S https://clients2.google.com/service/update2/crx\?response\=redirect\&acceptformat\=crx2,crx3\&prodversion\=133\&x\=id%3D${line}%26installsource%3Dondemand%26uc -o /tmp/extensions.crx)
   hash=$(nix-prefetch-url $finalurl)
-  hash=$(nix hash convert --to sri --hash-algo sha256 "$hash")
+  hash=$(nix hash to-sri --type sha256 "$hash")
   echo "{id=\"$line\";crxPath=pkgs.fetchurl{url = \"$finalurl\";hash = \"$hash\";};}"
 done < ./extensions-list
 
