e9bba27c9133859f9c2d15b4307bcf99f1122dd6
wxt <3264117476@qq.com>
2025-02-22 16:10:54 +0800

    Update

--- programs/chromium/update.sh
+++ programs/chromium/update.sh
@@ -5,9 +5,13 @@ while read line2
 do
   line=$(echo $line2 | awk -F " " '{print $1}')
   finalurl=$(curl -w "%{url_effective}\n" -I -L -s -S https://clients2.google.com/service/update2/crx\?response\=redirect\&acceptformat\=crx2,crx3\&prodversion\=133\&x\=id%3D${line}%26installsource%3Dondemand%26uc -o /tmp/extensions.crx)
-  hash=$(nix-prefetch-url $finalurl)
+  wget -O /tmp/extensions.zip $finalurl > /dev/null
+  hash=$(sha256sum /tmp/extensions.zip| cut -d" " -f1)
   hash=$(nix hash to-sri --type sha256 "$hash")
-  echo "{id=\"$line\";crxPath=pkgs.fetchurl{url = \"$finalurl\";hash = \"$hash\";};version = \"0.0.0\";}"
+  rm -rf /tmp/extensions > /dev/null
+  unzip /tmp/extensions.zip -d /tmp/extensions > /dev/null
+  version=$(jq -r ".version" < /tmp/extensions/manifest.json)
+  echo "{id=\"$line\";crxPath=pkgs.fetchurl{url = \"$finalurl\";name = \"$line.crx\";hash = \"$hash\";};version = \"$version\";}"
 done < ./extensions-list
 
 echo \]
