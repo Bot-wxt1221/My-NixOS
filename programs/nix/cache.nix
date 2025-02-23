0fcf8d468c305fb2a936ab0d8b73c8d36985388e
wxt <3264117476@qq.com>
2025-02-24 07:25:58 +0800

    Update

--- programs/nix/cache.nix
+++ programs/nix/cache.nix
@@ -14,9 +14,7 @@
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
     "https://wxt1221.cachix.org"
-    "https://cuda-maintainers.cachix.org"
     "https://luogu-judge.cachix.org"
-    "https://hyprland.cachix.org"
     "https://niri.cachix.org"
     "https://cache.garnix.io"
   ];
@@ -27,9 +25,7 @@
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "wxt1221.cachix.org-1:JmeZ+5reEo1i5qBc6X+2hEadoN5GFqTOQcwaGD5PEwY="
-    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     "luogu-judge.cachix.org-1:vBoiA0n//KfYZ5Xp5HhcujXcdT7dK3+rEK3U4zw66TQ="
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
