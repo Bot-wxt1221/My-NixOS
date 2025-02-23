acc487762164eb13a0c513df685a80b0b5de8906
wxt <3264117476@qq.com>
2025-02-23 12:18:54 +0800

    Update

--- home/wxt/default-g3.nix
+++ home/wxt/default-g3.nix
@@ -38,4 +38,9 @@
     };
   };
   programs.git.signing.format = lib.mkForce "openpgp";
+  audioProcess = ''
+    sleep 5
+    ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" set-default && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" mute && ponymix -t source  --device "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source" unmute
+    ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" set-default && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" mute && ponymix -t sink  --device "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" unmute
+  '';
 }
--- home/wxt/desktop/niri.nix
+++ home/wxt/desktop/niri.nix
@@ -24,112 +24,121 @@ in
 {
   imports = [
   ];
-  home.packages = with pkgs; [
-    fuzzel
-    xsel
-    pkgs.swaylock-effects
-    clipboard.packages.${pkgs.system}.default
-  ];
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    x11.defaultCursor = "macOS";
-    package = pkgs.apple-cursor;
-    name = "macOS";
-  };
-  xdg.enable = true;
-  xdg.portal = with pkgs; {
-    enable = true;
-    configPackages = [
-      wlr-xdg-desktop
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal
-    ];
-    extraPortals = [
-      wlr-xdg-desktop
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal
-    ];
-    config = {
-      niri = {
-        default = [
-          "gnome"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.RemoteDesktop" = [ "wlr" ];
-      };
+  options = {
+    audioProcess = lib.mkOption {
+      type = lib.types.str;
+      default = "";
     };
-    xdgOpenUsePortal = true;
   };
-  home.activation.mimeapps-remove = lib.hm.dag.entryBefore [
-    "checkLinkTargets"
-  ] ''rm -f ${config.home.homeDirectory}/.config/mimeapps.list'';
-  xdg.mimeApps =
-    let
-      br = "microsoft-edge.desktop";
-      fm = "org.gnome.Nautilus.desktop";
-      iv = "microsoft-edge.desktop";
-      mp = "microsoft-edge.desktop";
-      te = "nvim.desktop";
-    in
-    rec {
+  config = {
+    home.packages = with pkgs; [
+      fuzzel
+      xsel
+      pkgs.swaylock-effects
+      clipboard.packages.${pkgs.system}.default
+    ];
+    home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      x11.defaultCursor = "macOS";
+      package = pkgs.apple-cursor;
+      name = "macOS";
+    };
+    xdg.enable = true;
+    xdg.portal = with pkgs; {
       enable = true;
-      associations.added = defaultApplications;
-      defaultApplications = {
-        # Office documents.
-        "application/pdf" = br;
-
-        "inode/directory" = fm;
-
-        # Web stuff.
-        "application/xhtml+xml" = br;
-        "text/html" = br;
-        "x-scheme-handler/http" = br;
-        "x-scheme-handler/https" = br;
-
-        # Images.
-        "image/avif" = iv;
-        "image/gif" = iv;
-        "image/jpeg" = iv;
-        "image/jpg" = iv;
-        "image/pjpeg" = iv;
-        "image/png" = iv;
-        "image/tiff" = iv;
-        "image/webp" = iv;
-        "image/x-bmp" = iv;
-        "image/x-gray" = iv;
-        "image/x-icb" = iv;
-        "image/x-ico" = iv;
-        "image/x-png" = iv;
-
-        # Plain text & code.
-        "application/x-shellscript" = te;
-        "text/plain" = te;
-
-        # Videos.
-        "video/mkv" = mp;
-        "video/mp4" = mp;
-        "video/webm" = mp;
+      configPackages = [
+        wlr-xdg-desktop
+        xdg-desktop-portal-gnome
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal
+      ];
+      extraPortals = [
+        wlr-xdg-desktop
+        xdg-desktop-portal-gnome
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal
+      ];
+      config = {
+        niri = {
+          default = [
+            "gnome"
+            "gtk"
+          ];
+          "org.freedesktop.impl.portal.RemoteDesktop" = [ "wlr" ];
+        };
       };
+      xdgOpenUsePortal = true;
     };
-  xdg.configFile."niri/config.kdl".text = builtins.readFile (
-    pkgs.substituteAll {
-      src = ./niri.kdl;
-      authAgent = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
-      startXwayland = pkgs.writeText "a.sh" ''
-        sleep 3
-        xwayland-satellite :0
-      '';
-      startClashVerge = pkgs.writeText "a.sh" ''
-        sleep 5
-        clash-verge
-      '';
-      swaylockscript = pkgs.writeText "a.sh" ''
-        niri msg action power-off-monitors
-        ${lib.getExe pkgs.swaylock-effects} --screenshots --clock --font "WenQuanYi Micro Hei"
-      '';
-    }
-  );
+    home.activation.mimeapps-remove = lib.hm.dag.entryBefore [
+      "checkLinkTargets"
+    ] ''rm -f ${config.home.homeDirectory}/.config/mimeapps.list'';
+    xdg.mimeApps =
+      let
+        br = "microsoft-edge.desktop";
+        fm = "org.gnome.Nautilus.desktop";
+        iv = "microsoft-edge.desktop";
+        mp = "microsoft-edge.desktop";
+        te = "nvim.desktop";
+      in
+      rec {
+        enable = true;
+        associations.added = defaultApplications;
+        defaultApplications = {
+          # Office documents.
+          "application/pdf" = br;
+
+          "inode/directory" = fm;
+
+          # Web stuff.
+          "application/xhtml+xml" = br;
+          "text/html" = br;
+          "x-scheme-handler/http" = br;
+          "x-scheme-handler/https" = br;
+
+          # Images.
+          "image/avif" = iv;
+          "image/gif" = iv;
+          "image/jpeg" = iv;
+          "image/jpg" = iv;
+          "image/pjpeg" = iv;
+          "image/png" = iv;
+          "image/tiff" = iv;
+          "image/webp" = iv;
+          "image/x-bmp" = iv;
+          "image/x-gray" = iv;
+          "image/x-icb" = iv;
+          "image/x-ico" = iv;
+          "image/x-png" = iv;
+
+          # Plain text & code.
+          "application/x-shellscript" = te;
+          "text/plain" = te;
+
+          # Videos.
+          "video/mkv" = mp;
+          "video/mp4" = mp;
+          "video/webm" = mp;
+        };
+      };
+    xdg.configFile."niri/config.kdl".text = builtins.readFile (
+      pkgs.substituteAll {
+        src = ./niri.kdl;
+        authAgent = "${pkgs.pantheon.pantheon-agent-polkit}/libexec/policykit-1-pantheon/io.elementary.desktop.agent-polkit";
+        startXwayland = pkgs.writeText "a.sh" ''
+          sleep 3
+          xwayland-satellite :0
+        '';
+        startClashVerge = pkgs.writeText "a.sh" ''
+          sleep 5
+          clash-verge
+        '';
+        swaylockscript = pkgs.writeText "a.sh" ''
+          niri msg action power-off-monitors
+          ${lib.getExe pkgs.swaylock-effects} --screenshots --clock --font "WenQuanYi Micro Hei"
+        '';
+        SetupAudio = pkgs.writeText "a.sh" config.audioProcess;
+      }
+    );
+  };
 }
