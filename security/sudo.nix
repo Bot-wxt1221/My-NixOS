7ce2498f2568fb3dfed07affeff36bc959f37a6c
wxt <3264117476@qq.com>
2025-03-04 21:04:50 +0800

    Update

--- security/sudo.nix
+++ security/sudo.nix
@@ -8,6 +8,7 @@
   imports = [ ];
   security.sudo.wheelNeedsPassword = true;
   systemd.services.idontwanttoreadstupidsudolecture = {
+    enable = true;
     description = "I don't want to read stupid sudo lecture";
     serviceConfig.Type = "oneshot";
     script = ''
