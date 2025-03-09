5e887e5507ed787c15e2ffe2f1ad0e830714eccb
wxt <3264117476@qq.com>
2025-03-09 20:12:46 +0800

    Update

--- home/wxt/programs/konsole.nix
+++ home/wxt/programs/konsole.nix
@@ -11,11 +11,24 @@
   home.file.".local/share/konsole/Profile 1.profile" = {
     text = ''
       [Appearance]
-      Font=JetBrains Mono,13.5,-1,5,50,0,0,0,0,0
+      BorderWhenActive=false
+      ColorScheme=Breeze
+      Font=JetBrains Mono,14.7,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+      LineSpacing=0
+      UseFontLineChararacters=false
 
       [General]
+      DimWhenInactive=false
       Name=Profile 1
-      Parent=
+
+      [Interaction Options]
+      MiddleClickPasteMode=1
+
+      [Scrolling]
+      HistoryMode=2
+
+      [Terminal Features]
+      VerticalLine=false
     '';
   };
   home.file.".config/konsolerc" = {
@@ -27,14 +40,9 @@
       ConfigVersion=1
 
       [MainWindow]
-      1920x1080 screen: Height=982
-      1920x1080 screen: Width=934
       RestorePositionForNextInstance=false
       State=AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAvsAAAAiAFEAdQBpAGMAawBDAG8AbQBtAGEAbgBkAHMARABvAGMAawAAAAAA/////wAAAYMBAAAD+wAAABwAUwBTAEgATQBhAG4AYQBnAGUAcgBEAG8AYwBrAAAAAAD/////AAABIQEAAAMAAAOmAAADpwAAAAQAAAAEAAAACAAAAAj8AAAAAQAAAAIAAAACAAAAFgBtAGEAaQBuAFQAbwBvAGwAQgBhAHIBAAAAAP////8AAAAAAAAAAAAAABwAcwBlAHMAcwBpAG8AbgBUAG8AbwBsAGIAYQByAQAAAOD/////AAAAAAAAAAA=
       ToolBarsMovable=Disabled
-
-      [UiSettings]
-      ColorScheme=
     '';
   };
 }
