4d00c752f308c5470b514f778471be210a051586
wxt <3264117476@qq.com>
2025-03-11 21:54:49 +0800

    Update

--- system/kernel.nix
+++ system/kernel.nix
@@ -17,4 +17,8 @@
     setEnvironment = true;
     pamMount = false;
   };
+  systemd.tmpfiles.settings."zswap" = {
+    "/sys/module/zswap/parameters/enabled"."w-".argument = "1";
+    "/sys/module/zswap/parameters/zpool"."w-".argument = "zsmalloc";
+  };
 }
