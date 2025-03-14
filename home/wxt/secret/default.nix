806511fc114ca71e1c118e99ad150363be980ede
wxt <3264117476@qq.com>
2025-03-14 22:20:38 +0800

    Update

--- home/wxt/secret/default.nix
+++ home/wxt/secret/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 
@@ -7,4 +8,8 @@
     ./2FA
   ];
   sops.age.keyFile = "/persist/home/wxt/.config/sops/age/key.txt";
+  home.activation.copy-keys = ''
+    mkdir -p /home/wxt/.config/sops/age/
+    cp ${config.sops.age.keyFile} /home/wxt/.config/sops/age/keys.txt
+  '';
 }
