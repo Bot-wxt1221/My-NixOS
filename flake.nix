2e92e0fadc6f36b0df18a84c8af89830e6c5036b
wxt <3264117476@qq.com>
2025-03-15 22:04:25 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Main config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     nix-colors.url = "github:misterio77/nix-colors";
     luogu-gcc.url = "github:luogu-dev/judge-env";
--- nixos-modules/programs/rustdesk/default.nix
+++ nixos-modules/programs/rustdesk/default.nix
@@ -6,21 +6,21 @@
   imports = [
 
   ];
-  systemd.services.rustdesk = {
-    enable = true;
-    description = "rustdesk";
-    serviceConfig = {
-      ExecStart = "${pkgs.rustdesk-flutter}/bin/rustdesk --service";
-      ExecStop = "pkill -f \"rustdesk --\"";
-      PIDFile = "/run/rustdesk.pid";
-      KillMode = "mixed";
-      TimeoutStopSec = "30";
-      User = "root";
-      LimitNOFILE = "100000";
-    };
-    wantedBy = [ "multi-user.target" ];
-    requires = [ "network-online.target" ];
-    after = [ "display-manager.service" ];
-  };
-  environment.systemPackages = with pkgs; [ rustdesk-flutter ];
+  #systemd.services.rustdesk = {
+  #  enable = true;
+  #  description = "rustdesk";
+  #  serviceConfig = {
+  #    ExecStart = "${pkgs.rustdesk-flutter}/bin/rustdesk --service";
+  #    ExecStop = "pkill -f \"rustdesk --\"";
+  #    PIDFile = "/run/rustdesk.pid";
+  #    KillMode = "mixed";
+  #    TimeoutStopSec = "30";
+  #    User = "root";
+  #    LimitNOFILE = "100000";
+  #  };
+  #  wantedBy = [ "multi-user.target" ];
+  #  requires = [ "network-online.target" ];
+  #  after = [ "display-manager.service" ];
+  #};
+  #environment.systemPackages = with pkgs; [ rustdesk-flutter ];
 }
