6c407154ec460a3d4b5c99a50431f9c49d81b26b
wxt <3264117476@qq.com>
2025-03-16 12:56:56 +0800

    Update

--- home/home-modules/persist-nixosModules.nix
+++ home/home-modules/persist-nixosModules.nix
@@ -6,6 +6,12 @@
   imports = [
     ./programs/persist-default.nix
   ];
+  systemd.tmpfiles.settings.preservation = {
+    "/home/${config.home.username}/.config".d = { user = "${config.home.username}"; group = "users"; mode = "0755"; };
+    "/home/${config.home.username}/.local".d = { user = "${config.home.username}"; group = "users"; mode = "0755"; };
+    "/home/${config.home.username}/.local/share".d = { user = "${config.home.username}"; group = "users"; mode = "0755"; };
+    "/home/${config.home.username}/.local/state".d = { user = "${config.home.username}"; group = "users"; mode = "0755"; };
+  };
   preservation.preserveAt."/persist".users.wxt = {
     commonMountOptions = [
       "x-gvfs-hide"
