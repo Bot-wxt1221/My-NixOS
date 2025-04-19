c10ccd50aa5a686abd4a6af0502f4d7164160b5c
wxt <3264117476@qq.com>
2025-04-19 14:46:03 +0800

    Update

--- nixos-modules/hardware/kernel.nix
+++ nixos-modules/hardware/kernel.nix
@@ -15,6 +15,7 @@
     setEnvironment = true;
     pamMount = false;
   };
+  services.fwupd.enable = true;
   systemd.tmpfiles.settings."zswap" = {
     "/sys/module/zswap/parameters/enabled"."w-".argument = "1";
     "/sys/module/zswap/parameters/zpool"."w-".argument = "zsmalloc";
