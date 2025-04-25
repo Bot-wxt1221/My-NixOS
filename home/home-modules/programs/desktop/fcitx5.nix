39cc07e3734a6b5afbedb931ff71ff6c9f5d22fd
wxt <3264117476@qq.com>
2025-04-25 22:56:48 +0800

    Update

--- home/home-modules/programs/desktop/fcitx5.nix
+++ home/home-modules/programs/desktop/fcitx5.nix
@@ -6,7 +6,8 @@
 }:
 {
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = osConfig.i18n.inputMethod.fcitx5.addons;
     fcitx5.waylandFrontend = true;
     fcitx5.fcitx5-with-addons = pkgs.kdePackages.fcitx5-with-addons;
