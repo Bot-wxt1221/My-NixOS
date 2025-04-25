5f3c676a8a8f0685538e3d60e2dc15a502aab624
wxt <3264117476@qq.com>
2025-04-25 22:43:31 +0800

    Update

--- home/wxt/secret/2FA/default.nix
+++ home/wxt/secret/2FA/default.nix
@@ -7,13 +7,36 @@
   sops.secrets.passwordDateBase = {
     format = "binary";
     sopsFile = ./pas2s.kdbx.after;
-    path = "${config.home.homeDirectory}/2FA/pas2s.kdbx";
+    path = "${config.home.homeDirectory}/2FA/pas2s.kdbx.before";
     mode = "0400";
   };
   sops.secrets.passwordDateBaseSecret = {
     format = "binary";
     sopsFile = ./kee.after;
-    path = "${config.home.homeDirectory}/2FA/kee";
+    path = "${config.home.homeDirectory}/2FA/kee.before";
     mode = "0400";
+  };
+  systemd.user.services.keepassxc = {
+    Unit = {
+      Description = "keepassxc password manager";
+      Wants = [ "tray.target" ];
+      Requires = [ "sops-nix.service" ];
+      After = [
+        "sops-nix.service"
+        "tray.target"
+      ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Install.WantedBy = [ "xdg-desktop-autostart.target" ];
+
+    Service = {
+      Type = "exec";
+      Restart = "on-abnormal";
+      ExecStart = ''
+        install -Dm600 ${config.home.homeDirectory}/2FA/kee.before ${config.home.homeDirectory}/2FA/kee
+        install -Dm600 ${config.home.homeDirectory}/2FA/pas2s.kdbx.before ${config.home.homeDirectory}/2FA/pas2s.kdbx
+      '';
+    };
   };
 }
