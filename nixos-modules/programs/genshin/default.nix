0f74dea8f9fc6b613cb756611f7d19d32e910455
wxt <3264117476@qq.com>
2025-07-12 14:45:47 +0800

    Update

--- nixos-modules/programs/genshin/default.nix
+++ nixos-modules/programs/genshin/default.nix
@@ -4,9 +4,25 @@
   ...
 }:
 
+with pkgs;
 {
   imports = [
     an-anime-game-launcher.nixosModules.default
   ];
   programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
+  environment.systemPackages = [
+    (buildFHSEnv {
+      pname = "anime-game-launcher-wrapped";
+      version = an-anime-game-launcher.packages.${pkgs.system}.an-anime-game-launcher.version;
+      targetPkgs = pkgs: [
+        an-anime-game-launcher.packages.${pkgs.system}.an-anime-game-launcher
+        gamescope
+      ];
+      extraBwrapArgs = [
+        "--hostname steamdeck"
+        "--unshare-uts"
+      ];
+      runScript = "anime-game-launcher"; # a small script to choose what to exec
+    })
+  ];
 }
