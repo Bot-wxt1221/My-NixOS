1f8ef5e89fa0661cddc18e9d34627ed9d157ff99
wxt <3264117476@qq.com>
2025-09-19 23:43:07 +0800

    Update

--- home/home-modules/programs/desktop/swaync/default.nix
+++ home/home-modules/programs/desktop/swaync/default.nix
@@ -14,11 +14,6 @@
     };
     style = ./swaync.css;
   };
-  systemd.user.services.swaync = {
-    Unit = {
-      X-Restart-Triggers = lib.mkForce [ ];
-    };
-  };
   xdg.configFile."swaync/config.json".onChange = lib.mkForce "";
   xdg.configFile."swaync/style.css".onChange = lib.mkForce "";
 }
