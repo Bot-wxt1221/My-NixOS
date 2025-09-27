65405e68c67b7e8ccc5a1424e0de59f5929aaac8
wxt <3264117476@qq.com>
2025-09-27 14:21:38 +0800

    qq: use FHSEnv

--- nixos-modules/programs/tailscale/default.nix
+++ nixos-modules/programs/tailscale/default.nix
@@ -9,7 +9,7 @@
     mode = "0444";
   };
   services.tailscale = {
-    enable = true;
+    enable = false;
     extraUpFlags = [
       "--advertise-exit-node"
       "--advertise-routes=192.168.10.0/23,192.168.20.0/23,192.168.30.0/24,192.168.40.0/23,192.168.99.0/24"
