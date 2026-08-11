0395bb38d8ea65ecc8a9c8f5c7935ed2b5a8a872
wxt <3264117476@qq.com>
2026-08-11 16:18:32 +0800

    a

--- nixos-modules/hardware/kernel.nix
+++ nixos-modules/hardware/kernel.nix
@@ -11,10 +11,6 @@
   boot.kernel.sysctl = {
     "kernel.sysrq" = 255;
   };
-  security.pam.services.systemd-run0 = {
-    setEnvironment = true;
-    pamMount = false;
-  };
   services.fwupd.enable = true;
   systemd.tmpfiles.settings."zswap" = {
     "/sys/module/zswap/parameters/enabled"."w-".argument = "1";
--- nixos-modules/security/sudo.nix
+++ nixos-modules/security/sudo.nix
@@ -1,26 +1,16 @@
-{
-  pkgs,
-  ...
-}:
-{
-  imports = [ ];
-  security.sudo.wheelNeedsPassword = true;
-  systemd.services.idontwanttoreadstupidsudolecture = {
-    enable = true;
-    description = "I don't want to read stupid sudo lecture";
-    serviceConfig.Type = "oneshot";
-    script = ''
-      mkdir -p /var/db/sudo/lectured 
-      touch /var/db/sudo/lectured/1000
-    '';
-    wantedBy = [ "multi-user.target" ];
-  };
-  security.wrappers = {
-    pkexec = {
-      setuid = true;
-      owner = "root";
-      group = "root";
-      source = "${pkgs.polkit.bin}/bin/pkexec";
+_: {
+  security = {
+    sudo.enable = false;
+    polkit = {
+      enable = true;
+      enablePkexecWrapper = true;
+    };
+    run0 = {
+      enable = true;
+      sudo-shim = {
+        enable = true;
+      };
     };
   };
+  system.tools.nixos-rebuild.enableRun0Elevation = true;
 }
